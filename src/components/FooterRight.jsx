9e4171435a77f6bb4d0e1863a84aa35f684d8c80
tuyetmai <duongmai6241@gmail.com>
2024-11-15 10:13:37 +0700

    cau1

--- src/components/FooterRight.jsx
+++ src/components/FooterRight.jsx
@@ -1,12 +1,112 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus, faCircleCheck, faHeart, faCommentDots, faBookmark, faShare } from '@fortawesome/free-solid-svg-icons';
-import './FooterRight.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlus,
+  faCircleCheck,
+  faHeart,
+  faCommentDots,
+  faBookmark,
+  faShare,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import "./FooterRight.css";
 
 function FooterRight({ likes, comments, saves, shares, profilePic }) {
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const [userAddIcon, setUserAddIcon] = useState(faCirclePlus);
 
+  const handleUserAddClick = () => {
+    setUserAddIcon(faCircleCheck);
+    setTimeout(() => {
+      setUserAddIcon(null);
+    }, 3000);
+  };
+  const parseLikesCount = (count) => {
+    if (typeof count == "string") {
+      if (count.endsWith("K")) {
+        return parseFloat(count) * 1000;
+      }
+      return parseInt(count);
+    }
+    return count;
+  };
+  const formatLikeCount = (count) => {
+    if (count >= 1000) {
+      return (count / 1000).toFixed(1) + "K";
+    }
+    return count;
+  };
+  const hancleLikeClick = () => {
+    setLiked((prevLiked) => !prevLiked);
+  };
   return (
-
+    <div className="footer-right">
+      <div className="sidebar-icon">
+        {profilePic ? (
+          <img
+            src={profilePic}
+            className="userprofile"
+            alt="Profile"
+            style={{ width: "45px", height: "45px", color: "#616161" }}
+          />
+        ) : null}
+        <FontAwesomeIcon
+          icon={userAddIcon}
+          className="useradd"
+          style={{ width: "15px", height: "15px", color: "#ff0000" }}
+          onClick={handleUserAddClick}
+        />
+      </div>
+      <div className="sidebar-icon">
+        <FontAwesomeIcon
+          icon={faHeart}
+          style={{
+            width: "35px",
+            height: "35px",
+            color: liked ? "#ff0000" : "white",
+          }}
+          onClick={hancleLikeClick}
+        />
+        <p>{formatLikeCount(parseLikesCount(likes) + (liked ? 1 : 0))}</p>
+      </div>
+      <div className="sidebar-icon">
+        <FontAwesomeIcon
+          icon={faCommentDots}
+          style={{ width: "35px", height: "35px", color: "white" }}
+        />
+        <p>{comments}</p>
+      </div>
+      <div className="sidebar-icon">
+        {saved ? (
+          <FontAwesomeIcon
+            icon={faBookmark}
+            style={{ width: "35px", height: "35px", color: "#ffc107" }}
+            onClick={() => setSaved(false)}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faBookmark}
+            style={{ width: "35px", height: "35px", color: "white" }}
+            onClick={() => setSaved}
+          />
+        )}
+        <p>{saved ? saves + 1 : saves}</p>
+      </div>
+      <div className="sidebar-icon">
+        <FontAwesomeIcon
+          icon={faShare}
+          style={{ width: "35px", height: "35px", color: "white" }}
+        />
+        <p>{shares}</p>
+        <div className="sidebar-icon record">
+          <img
+            src="https://static.thenounproject.com/png/934821-200.png"
+            alt="Record icon"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
